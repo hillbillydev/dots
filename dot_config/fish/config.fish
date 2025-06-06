9b5d2bbe13da99b73f98fe6da49d35c04511ca36
Marcus Karlsson <marcus.karlsson@lightspeedhq.com>
2025-06-07 09:27:24 +1200

    refactor(fish): remove private prefix

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,32 @@
+set -x GOPATH "$HOME/go"
+set -x EDITOR "zed --wait"
+set -g fish_greeting ""
+set -gx fish_history_search_ignore_case true
+
+fish_add_path \
+    /opt/homebrew/bin \
+    /opt/homebrew/sbin \
+    /usr/local/bin \
+    /usr/local/sbin \
+    /usr/bin \
+    /bin \
+    /usr/sbin \
+    /Library/Apple/usr/bin \
+    $HOME/.local/bin \
+    $GOPATH/bin
+
+fnm env | source
+zoxide init fish | source
+starship init fish | source
+
+fish_default_key_bindings
+
+bind \cz '__zoxide_zi'
+
+alias ls="eza --icons --git"
+alias cat="bat"
+alias grep="rg"
+alias g="git"
+alias ghmerge="gh pr merge --squash --delete-branch"
+alias ghview="gh pr view --web"
+alias lg="lazygit"
